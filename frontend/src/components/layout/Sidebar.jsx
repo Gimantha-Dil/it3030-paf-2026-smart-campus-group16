92f340f94011dbef61637fd624ca3b449f3c8b55
Sasmika Rupasingha <sasmikarupasingha@gmail.com>
2026-04-16 12:47:29 +0530

    Add sidebar component structure

---
+++ frontend/src/components/layout/Sidebar.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Sidebar({ open, onClose }) {
+  return (
+    <aside>
+      <p>Sidebar</p>
+    </aside>
+  );
+}
